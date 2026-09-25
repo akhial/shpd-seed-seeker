186b942306804d2f23e4a7218b834daf6f1a967c
Adel Khial <adel.khial@gmail.com>
2026-09-25 20:18:28 +0200

    Release v0.18.0

--- web/src/designs/one/App.tsx
+++ web/src/designs/one/App.tsx
@@ -421,7 +421,7 @@ export default function App() {
             title="View source on GitHub"
           >
             <span className="d1-mono">
-              <span className="d1-gh-name">SHPD Seed Seeker </span>v0.17.1
+              <span className="d1-gh-name">SHPD Seed Seeker </span>v0.18.0
             </span>
             <span className="d1-gh-icon" aria-hidden="true" />
           </a>
